55bd792c931da0ae4769c23f36703c080ff0ee3c
sadbatya <sadbatya@mail.ru>
2024-07-17 19:08:11 -0400

    добавил переключение языка

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,40 @@
-import { Switch } from 'antd'
-import { MoonOutlined, SunOutlined } from '@ant-design/icons'
-import style from './header.module.css'
-import { useTheme } from '../providers/ThemeProvider'
-import { useTranslation } from 'react-i18next'
+import { Switch } from "antd";
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
+import style from "./header.module.css";
+import { useTheme } from "../providers/ThemeProvider";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+
 
 const Header: React.FC = () => {
-  const { theme, toggleTheme} = useTheme()
-  const { t,i18n } = useTranslation()
+  const { theme, toggleTheme } = useTheme();
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
 
-  
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setLanguage(lang);
+  };
 
   return (
     <div className={style.header}>
-      <span>Время недели</span>
+      <span>{t("main.header")}</span>
       <div className={style.switchers}>
-        <Switch checkedChildren='EN' unCheckedChildren='RU'/>
-        <Switch onChange={toggleTheme} checkedChildren={<SunOutlined />} unCheckedChildren={<MoonOutlined />}  defaultChecked/>
+        <Switch
+          checkedChildren="EN"
+          unCheckedChildren="RU"
+          onChange={() => changeLanguage(language === "ru" ? "en" : "ru")}
+          checked={language === "en"}
+        />
+        <Switch
+          onChange={toggleTheme}
+          checkedChildren={<SunOutlined />}
+          unCheckedChildren={<MoonOutlined />}
+          defaultChecked
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default Header
+export default Header;
